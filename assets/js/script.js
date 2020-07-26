a252e8ad13efee30f0ab5381c1d773cbbd2ffb74
PedrooLska <pedrolucas81@hotmail.com>
2020-07-26 12:17:32 -0300

    Refatoração de Código e Bem Architeture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,67 @@
-class AnalisadorNumeros {
-    constructor() {
-        this.number = document.querySelector('.number');
-        this.resultNumber = document.querySelector('.resultNumber');
-        this.result = document.querySelector('.result');
+const number = document.querySelector('#number');
+const resultNumber = document.querySelector('#resultNumber');
+const result = document.querySelector('#result');
+const erro = document.querySelector('#erro');
 
-        this.arrayNumber = [];
+const arrayNumber = [];
+
+const validationNumber = (num) => {
+    if (num >= 1 && num <= 100) {
+        return true
+    } else if (num.length == 0) {
+        erroMsg('Por favor preencha o campo.');
+        clearInput();
+    } else {
+        erroMsg('Por favor preencha com números entre 1 é 100.');
+        clearInput();
     }
+}
 
-    validationNumber(num) {
-        if (num >= 1 && num <= 100) {
-            return true
-        } else if (num.length == 0) {
-            this.erroMsg('Por favor preencha o campo.');
-            this.clearInput();
-        } else {
-            this.erroMsg('Por favor preencha com números entre 1 é 100.');
-            this.clearInput();
-        }
+const validateDuplicateNumber = (l, n) => {
+    if (l.indexOf(Number(n)) === -1) {
+        erroMsg('');
+        return true
+    } else {
+        erroMsg('Número já cadastrado');
+        clearInput();
+    };
+}
+
+const addNumber = () => {
+    if (validationNumber(number.value) && validateDuplicateNumber(arrayNumber, number.value)) {
+        resultNumber.style.display = 'block'
+        arrayNumber.push(Number(number.value));
+        resultNumber.innerHTML += `${number.value} `;
+        clearInput();
     }
+}
 
-    validateDuplicateNumber(l, n) {
-        if (l.indexOf(Number(n)) === -1) {
-            this.erroMsg('');
-            return true
-        } else {
-            this.erroMsg('Número já cadastrado');
-            this.clearInput();
-        };
-    }
-
-    addNumber() {
-        if (this.validationNumber(this.number.value) && this.validateDuplicateNumber(this.arrayNumber, this.number.value)) {
-            this.arrayNumber.push(Number(this.number.value));
-            this.resultNumber.innerHTML += `${this.number.value} `;
-            this.clearInput();
-        }
-    }
-
-    analiseNumber() {
-        if (this.arrayNumber.length >= 2) {
-            this.erroMsg('');
-            this.result.innerHTML = '';
-            let lenght = this.arrayNumber.length;
-            let max = Math.max.apply(Math, this.arrayNumber);
-            let min = Math.min.apply(Math, this.arrayNumber);
-            let addition = this.arrayNumber.reduce((a, b) => a + b)
-            let media = this.arrayNumber.reduce((a, b) => a + b) / lenght;
-
-            this.result.innerHTML += `<p>Ao todo, temos <strong>${lenght}</strong> números cadastrados.</p> 
+const analiseNumber = () => {
+    if (arrayNumber.length >= 2) {
+        erroMsg('');
+        result.innerHTML = '';
+        let lenght = arrayNumber.length;
+        let max = Math.max.apply(Math, arrayNumber);
+        let min = Math.min.apply(Math, arrayNumber);
+        let addition = arrayNumber.reduce((a, b) => a + b)
+        let media = arrayNumber.reduce((a, b) => a + b) / lenght;
+        result.style.display = 'block'
+        result.innerHTML += `<p>Ao todo, temos <strong>${lenght}</strong> números cadastrados.</p> 
                 <p> O maior valor informado foi <strong> ${max}</strong >.</p >
                 <p>O menor valor informado foi <strong>${min}</strong>.</p> 
                 <p>Somando todos os valores, temos <strong>${addition}</strong>.</p>
                 <p>A média dos valores digitados é <strong>${media.toFixed(2)}</strong></p>`
-        } else {
-            this.erroMsg('Por favor preencha com ao menos 2 números para podermos analisar.');
-        }
+    } else {
+        erroMsg('Por favor preencha com ao menos 2 números para podermos analisar.');
     }
-
-    erroMsg(msg) {
-        let el = document.querySelector('.erro');
-        el.innerHTML = msg;
-    }
-
-    clearInput() {
-        this.number.value = '';
-        this.number.focus();
-    }
-
-
-
-
-
-
-
 }
 
-const newAnalisadorNumeros = new AnalisadorNumeros();
+const erroMsg = (msg) => {
+    erro.style.display = 'block'
+    erro.innerHTML = msg;
+}
+
+const clearInput = () => {
+    this.number.value = '';
+    this.number.focus();
+}
